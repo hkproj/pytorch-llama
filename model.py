2c3ee0111c69b580190acb83af21c323c9e8e628
Umar Jamil <hkproj@gmail.com>
2023-09-01 13:26:14 +0800

    simplified rotary pos embeddings

--- model.py
+++ model.py
@@ -41,38 +41,35 @@ class RMSNorm(nn.Module):
         return self.weight * self._norm(x.float()).type_as(x)
 
 
-def precompute_theta_pos_frequencies(dim: int, seq_len: int, device: str, theta: float = 10000.0):
+def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, theta: float = 10000.0):
     # As written in the paragraph 3.2.2 of the paper
     # >> In order to generalize our results in 2D to any xi ∈ Rd where **d is even**, [...]
-    assert dim % 2 == 0, "Dimension must be divisible by 2"
+    assert head_dim % 2 == 0, "Dimension must be divisible by 2"
     # Build the theta parameter
     # According to the formula theta_i = 10000^(-2(i-1)/dim) for i = [1, 2, ... dim/2]
-    theta_numerator = torch.arange(0, dim, 2).float()
-    theta = 1.0 / (theta ** (theta_numerator / dim)).to(device)
+    # Shape: (Head_Dim / 2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # Shape: (Head_Dim / 2)
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device) # (Dim / 2)
     # Construct the positions (the "m" parameter)
+    # Shape: (Seq_Len)
     m = torch.arange(seq_len, device=device)
     # Multiply each theta by each position using the outer product.
+    # Shape: (Seq_Len) outer_product* (Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
     freqs = torch.outer(m, theta).float()
     # We can compute complex numbers in the polar form c = R * exp(m * theta), where R = 1 as follows:
+    # (Seq_Len, Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_complex
-
-
-def reshape_for_broadcasting(freqs_complex: torch.Tensor, x: torch.Tensor):
-    # freqs_complex: (Seq_Len, Head_Dim/2)
-    # X: (B, Seq_Len, H, Head_Dim/2)
-    # So we need to add the batch dimension and the head dimension
-    return freqs_complex.unsqueeze(0).unsqueeze(2)
-
 
 def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
     # Separate the last dimension pairs of two values, representing the real and imaginary parts of the complex number
     # Two consecutive values will become a single complex number
     # (B, Seq_Len, H, Head_Dim) -> (B, Seq_Len, H, Head_Dim/2)
     x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
-    # Reshape the freqs_complex tensor to match the shape of the x_complex tensor
+    # Reshape the freqs_complex tensor to match the shape of the x_complex tensor. So we need to add the batch dimension and the head dimension
     # (Seq_Len, Head_Dim/2) --> (1, Seq_Len, 1, Head_Dim/2)
-    freqs_complex = reshape_for_broadcasting(freqs_complex, x_complex)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
     # Multiply each complex number in the x_complex tensor by the corresponding complex number in the freqs_complex tensor
     # Which results in the rotation of the complex number as shown in the Figure 1 of the paper
     # (B, Seq_Len, H, Head_Dim/2) * (1, Seq_Len, 1, Head_Dim/2) = (B, Seq_Len, H, Head_Dim/2)
@@ -257,7 +254,7 @@ class Transformer(nn.Module):
         self.norm = RMSNorm(args.dim, eps=args.norm_eps)
         self.output = nn.Linear(args.dim, self.vocab_size, bias=False)
 
-        self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, self.args.max_seq_len* 2, device=self.args.device)
+        self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device=self.args.device)
 
     def forward(self, tokens: torch.Tensor, start_pos: int):
         # (B, Seq_Len)
